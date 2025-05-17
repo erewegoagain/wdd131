f533966c943a921152dc116f138e800de251a2ba
erewegoagain <mcdougalpenguin@byui.edu>
2025-05-17 18:18:06 -0400

    First submission for coolpics enlarged images

---
+++ coolpics/coolpics.js
@@ -0,0 +1,40 @@
+const menuButton = document.querySelector(".dropbtn");
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog')
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+  const menu = document.querySelector(".menu");
+  if (window.innerWidth > 999) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+function enlargeImage(event) {
+    const image = event.target.closest('img');
+    const base = image.src.split('-')[0];
+    const final = base + '-full.jpeg';
+    modal.innerHTML = `
+    <img src="${final}" alt="${image.alt}">
+    <button class="close-viewer">X</button>
+  `;
+  modal.showModal();
+}
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
+})
+
+gallery.addEventListener('click', enlargeImage) 
